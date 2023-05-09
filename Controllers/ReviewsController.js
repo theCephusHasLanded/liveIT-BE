571cc29bf7374cdcd3f7eac9f4cc6f0f1d4eaa48
jkpasaoa <jacqueline.pasaoa@gmail.com>
2023-05-09 17:34:20 -0400

    adjusted review queries again

--- Controllers/ReviewsController.js
+++ Controllers/ReviewsController.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const reviews = express.Router({ mergeParams: true });
 
-//const { getASnack } = require("../queries/snacks.js");
-
 const {
   getAllReviews,
   getReview,
@@ -10,56 +8,50 @@ const {
   deleteReview,
   updateReview,
 } = require("../queries/reviews");
-//const { get } = require("../app.js");
 
-//All reviews for one snack
-snacks.use("/:snackId/reviews", reviewsController)
-
-//INDEX - get all reviews
+// Get all reviews
 reviews.get("/", async (req, res) => {
-  const { snackId } = req.params;
-  //(snackId);
   try {
-    const allReviews = await getAllReviews(snackId);
+    const allReviews = await getAllReviews();
     res.json(allReviews)
   } catch (err) {
     res.json(err)
   }
 });
 
-//SHOW - single review
+// Get all reviews for a specific snack
+reviews.get("/snack/:snackId/reviews", async (req, res) => {
+  const { snackId } = req.params;
+  try {
+    const allReviews = await getAllReviewsForOneSnack(snackId);
+    res.json(allReviews)
+  } catch (err) {
+    res.json(err)
+  }
+});
+
+// Get a single review
 reviews.get("/:id", async (req, res) => {
   const { id } = req.params;
   const review = await getReview(id);
   if (review) {
-    res.status.json(review);
-  } else {
-    res.status(404).json({ error: "not found" });
-  }
-});
-
-// CREATE - New Review
-reviews.post("/", async (req, res) => {
-  try {
-  const review = await createReview(req.body);
-  res.status(200).json(review);
-  } catch (error) {
-    res.status(400).json({ error: error })
-  }
-});
-
-// DELETE
-reviews.delete("/:id", async (req, res) => {
-  const { id } = req.params;
-  const deletedReview = await deleteReview(id);
-  if (deletedReview.id) {
-    res.status(200).json(deletedReview);
+    res.json(review);
   } else {
     res.status(404).json({ error: "Review not found" });
   }
 });
 
-// UPDATE - previously done review
+// Create a new review
+reviews.post("/", async (req, res) => {
+  try {
+    const review = await createReview(req.body);
+    res.status(200).json(review);
+  } catch (error) {
+    res.status(400).json({ error: error })
+  }
+});
+
+// Update a review
 reviews.put("/:id", async (req, res) => {
   const { id } = req.params;
   const updatedReview = await updateReview(id, req.body);
@@ -70,4 +62,96 @@ reviews.put("/:id", async (req, res) => {
   }
 });
 
+// Delete a review
+reviews.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  const deletedReview = await deleteReview(id);
+  if (deletedReview.id) {
+    res.status(200).json(deletedReview);
+  } else {
+    res.status(404).json({ error: "Review not found" });
+  }
+});
+
 module.exports = reviews;
+
+
+// const express = require("express");
+// const reviews = express.Router({ mergeParams: true });
+
+// //const { getASnack } = require("../queries/snacks.js");
+
+// const {
+//   getAllReviews,
+//   getReview,
+//   createReview,
+//   deleteReview,
+//   updateReview,
+// } = require("../queries/reviews");
+// //const { get } = require("../app.js");
+
+// //INDEX - get all reviews
+// reviews.get("/", async (req, res) => {
+//   try {
+//     const allReviews = await getAllReviews();
+//     res.json(allReviews)
+//   } catch (err) {
+//     res.json(err)
+//   }
+// });
+
+// //All reviews for one snack
+// reviews.get("/:snackId/reviews", async (req, res) => {
+//   const { snackId } = req.params;
+//   try {
+//     const allReviews = await getAllReviews(snackId);
+//     res.json(allReviews)
+//   } catch (err) {
+//     res.json(err)
+//   }
+// });
+
+// //SHOW - single review
+// reviews.get("/:id", async (req, res) => {
+//   const { id } = req.params;
+//   const review = await getReview(id);
+//   if (review) {
+//     res.status.json(review);
+//   } else {
+//     res.status(404).json({ error: "not found" });
+//   }
+// });
+
+// // CREATE - New Review
+// reviews.post("/", async (req, res) => {
+//   try {
+//   const review = await createReview(req.body);
+//   res.status(200).json(review);
+//   } catch (error) {
+//     res.status(400).json({ error: error })
+//   }
+// });
+
+// // DELETE
+// reviews.delete("/:id", async (req, res) => {
+//   const { id } = req.params;
+//   const deletedReview = await deleteReview(id);
+//   if (deletedReview.id) {
+//     res.status(200).json(deletedReview);
+//   } else {
+//     res.status(404).json({ error: "Review not found" });
+//   }
+// });
+
+// // UPDATE - previously done review
+// reviews.put("/:id", async (req, res) => {
+//   const { id } = req.params;
+//   const updatedReview = await updateReview(id, req.body);
+//   if (updatedReview.id) {
+//     res.status(200).json(updatedReview);
+//   } else {
+//     res.status(404).json("Review not found");
+//   }
+// });
+
+// module.exports = reviews;
--- queries/reviews.js
+++ queries/reviews.js
@@ -10,6 +10,11 @@ const getAllReviews = async (snackId) => {
   }
 };
 
+//get all reviews for one snack
+const getAllReviewsForOneSnack = async (id) => {
+  console.log("HI")
+}//select all reviews and grab snack_id not primary id
+
 //SHOW - single review
 const getReview = async (id) => {
   try {
@@ -33,12 +38,11 @@ const createReview = async (review) => {
   }
 };
 
-//Update - previously done review needs changes
 const updateReview = async (id, review) => {
   // const { reviewer, title, content, rating, snack_id } = review;
   try {
     const updatedReview = await db.one(
-      "UPDATE reviews SET reviewer=$1, title=$2, content=$3, rating=$4 snack_id=$5 WHERE id=$6 RETURNING *",
+      "UPDATE reviews SET reviewer=$1, title=$2, content=$3, rating=$4, snack_id=$5 WHERE id=$6 RETURNING *",
       [review.reviewer, review.title, review.content, review.rating, review.snack_id, id]
     );
     return updatedReview;
@@ -46,6 +50,21 @@ const updateReview = async (id, review) => {
     return error;
   }
 };
+
+
+// //Update - previously done review needs changes
+// const updateReview = async (id, review) => {
+//   // const { reviewer, title, content, rating, snack_id } = review;
+//   try {
+//     const updatedReview = await db.one(
+//       "UPDATE reviews SET reviewer=$1, title=$2, content=$3, rating=$4 snack_id=$5 WHERE id=$6 RETURNING *",
+//       [review.reviewer, review.title, review.content, review.rating, review.snack_id, id]
+//     );
+//     return updatedReview;
+//   } catch (error) {
+//     return error;
+//   }
+// };
 
 
 //DELETE - delete review

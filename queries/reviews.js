f498f3e30e2151bd05f4d8b52ddfc4c3db6c971f
jkpasaoa <jacqueline.pasaoa@gmail.com>
2023-05-12 13:08:26 -0400

    spacing in reviews

--- queries/reviews.js
+++ queries/reviews.js
@@ -33,7 +33,13 @@ const createReview = async (review) => {
   try {
     const newReview = await db.one(
       "INSERT INTO reviews (reviewer, title, content, rating, snack_id) VALUES($1, $2, $3, $4, $5) RETURNING *",
-      [review.reviewer, review.title, review.content, review.rating, review.snack_id]
+      [
+        review.reviewer, 
+        review.title, 
+        review.content, 
+        review.rating, 
+        review.snack_id,
+      ]
     );
     return newReview;
   } catch (error) {
@@ -46,7 +52,14 @@ const updateReview = async (id, review) => {
   try {
     const updatedReview = await db.one(
       "UPDATE reviews SET reviewer=$1, title=$2, content=$3, rating=$4, snack_id=$5 WHERE id=$6 RETURNING *",
-      [review.reviewer, review.title, review.content, review.rating, review.snack_id, id]
+      [
+        review.reviewer, 
+        review.title, 
+        review.content, 
+        review.rating, 
+        review.snack_id,
+        id
+      ]
     );
     return updatedReview;
   } catch (error) {
